0e98e51c0749e27335d8b59e6380ac72087e2a84
ulikses <ajarcev@seas.upenn.edu>
2020-09-06 20:40:30 +0000

    lab1 almost done

---
+++ lab1/c_programs/test4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = getchar();
+    if (a==1) {
+        a = getchar();
+        if (a==3) {
+            a = getchar();
+            if (a==5) {
+                a = getchar();
+                if (a==7) {
+                    a = getchar();
+                    a = 1/a;
+                }
+            }
+        }
+    }
+    return 0;
+}
