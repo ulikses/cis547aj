7d583de8b3a7213cc7b9af2515f8d867616453d5
ulikses <ajarcev@seas.upenn.edu>
2020-09-14 01:38:13 +0000

    lab1 done

---
+++ lab1/submission/c_programs/test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = getchar();
+    int b = 0;
+    int c = (a!=b);
+    int d = b/c;
+    return 0;
+}
---
+++ lab1/submission/c_programs/test3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = getchar();
+    int b = 0;
+    int c;
+    int d;
+    if (a>100) {
+        d = a/b;
+    }
+    return 0;
+}
